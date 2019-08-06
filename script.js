1556a8a0f3c6a4f20d182eac4c7afe1203ae32cc
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-06 13:18:08 +0200

    Popup de la partie « compétences ».

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ imagesLoaded( grid ).on( 'progress', function() {
 // layout Masonry after each image loads
 msnry.layout();
 });
+
+//POPUP
+function maquetteFunction() {
+  var maquettePopup = document.getElementById("maquettePopup");
+  maquettePopup.classList.toggle("show");
+}
+function interfaceStatiqueFunction() {
+  var interfacePopup = document.getElementById("interfaceStatiquePopup");
+  interfacePopup.classList.toggle("show");
+}
+function interfaceDynamiqueFunction() {
+  var interfacePopup = document.getElementById("interfaceDynamiquePopup");
+  interfacePopup.classList.toggle("show");
+}
+function cmsFunction() {
+  var interfacePopup = document.getElementById("cmsPopup");
+  interfacePopup.classList.toggle("show");
+}
+function bddFunction() {
+  var interfacePopup = document.getElementById("bddPopup");
+  interfacePopup.classList.toggle("show");
+}
+function accesFunction() {
+  var interfacePopup = document.getElementById("accesPopup");
+  interfacePopup.classList.toggle("show");
+}
+function backendFunction() {
+  var interfacePopup = document.getElementById("backendPopup");
+  interfacePopup.classList.toggle("show");
+}
+function composantsFunction() {
+  var interfacePopup = document.getElementById("composantsPopup");
+  interfacePopup.classList.toggle("show");
+}
